6fa2197b9e75dcc758770c17836c41e9149ee9e2
jwallace7 <jonawa6@mail.regent.edu>
2024-04-15 11:06:30 -0400

    Update bookDatabase.h
    Removed redundant addBook() and removeBook() functions

--- src/bookDatabase.h
+++ src/bookDatabase.h
@@ -13,19 +13,6 @@ class bookDatabase : public orderedLinkedList<book>
 public:
   void loadFromFile(ifstream inFile); // *Alyssa* Function to load the address book from a file
   void saveToFile(ofstream outFile); // *Alyssa* Function to save the address book to a file
-
-  void addBook(book newBook);
-  // Function to insert a book in the ordered linked list
-  // Precondition: the book object must already exist
-  // and be passed as a parameter.
-  // Postcondition: newBook will be part of the ordered
-  // linked list, and count will be incremented by 1.
-  void removeBook(book newBook);
-  // Function to remove a book from the ordered linked list.
-  // Precondition: The unique identifier we use for a book
-  // (i.e., the title, the dewey decimal) will be the only
-  // identifier needed, because of the way the '==' and
-  // '>=' operators are overloaded
   
 };
 

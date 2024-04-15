3489c895f43e4ee45a2347f78238012dbdcf82e3
pod-s <85466176+pod-s@users.noreply.github.com>
2024-04-15 09:14:05 -0400

    Updated book.h function headers and overloads
    Created definitions for the equality and greater than or equal to operators.
    Updated assignment operator definition to check for self-assignment.
    Fixed get function headers by placing const after the function declaration. This ensures the function itself is a constant function instead of the variable it is returning.
    Added definition for the printTitle() function.
    Added a copy constructor to the book class.
    Added a private function copyBook() which copies the book passed into the function into the current book. Used in the assignment operator and the copy constructor.

--- src/book.h
+++ src/book.h
@@ -11,9 +11,14 @@ class book
 {
 public:
   // OPERATOR OVERLOADING
-  const book& operator=(const book&);
-  bool operator==(const book&) const;
+  const book& operator=(const book&); // Assignment operator
+  bool operator==(const book&) const; 
+  // Equality operator. Used for sorting.
+  // Postcondition: Returns true if the titles are the same.
   bool operator>=(const book&) const;
+  // Greater than or equal to operator. Used for sorting.
+  // Postcondition: Returns true if the title of this book is greater than or
+  // equal to the other book.
 
   // SET FUNCTIONS
   void setTitle(string t); // Function to set title
@@ -23,35 +28,48 @@ public:
   void setBorrowStatus(bool stat); // Function to set borrowed
 
   // GET FUNCTIONS
-  const string getTitle(); // Function to get title
-  const string getAuthor(); // Function to get author name
-  const int getDate(); // Function to get datePublished
-  const bool isBorrowed(); // Function to get if the book is borrowed or not
-  const double getCatalogNumber(); // Function to get catalog number
+  string getTitle() const; // Function to get title
+  string getAuthor() const; // Function to get author name
+  int getDate() const; // Function to get datePublished
+  bool isBorrowed() const; // Function to get if the book is borrowed or not
+  double getCatalogNumber() const; // Function to get catalog number
 
-  const void printTitle(); // Function to print book title *use for selection options in search*
-  const void printInfo(); // *Alyssa* Function to print book information to console
-  const void printInfo(ofstream outFile); // *Alyssa* Function to print book information to an output file
+  // PRINT FUNCTIONS
+  void printTitle() const; // Function to print book title *use for selection options in search*
+  void printInfo() const; // *Alyssa* Function to print book information to console
+  void printInfo(ofstream outFile) const; // *Alyssa* Function to print book information to an output file
 
   // Constructor. Borrower always starts off as an empty string, and the book always starts off as not borrowed
-  book(string newTitle = "", string name = "", int publishDate = 1990);
+  book(string newTitle = "", string name = "", int publishDate = 1990, double num);
+
+  // Copy constructor. Makes an identical book to the book provided in the copy constructor.
+  book(const book&);
 
 private:
   string title, author, borrower; // Book title and author (Borrower will be implemented in phase 2)
   int datePublished = 1990; // Date published
   bool borrowed = false; // Boolean value to determine borrow status (implemented in phase 2)
   double catalogNumber; // Book catalog number, Dewey decimal system
+
+  void copyBook(const book&); // Private function for copying books.
 };
 
 // OPERATOR OVERLOADING
-const book& operator=(const book& otherBook)
+const book& book::operator=(const book& otherBook)
 {
-	title = otherBook.getTitle();
-	author = otherBook.getAuthor();
-	borrowed = otherBook.isBorrowed();
-	datePublished = otherBook.getDate();
-	catalogNumber = otherBook.getCatalogNumber();
-}
+  if (this != &otherBook) // Checks self-assignment
+  {
+    copyBook(otherBook);
+  }
+	
+  return *this;
+}//end assignment operator
+
+bool book::operator==(const book& otherBook) const
+{ return (title == otherBook.getTitle()); }//end equality operator
+
+bool book::operator>=(const book& otherBook) const
+{ return (title >= otherBook.getTitle()); }//end gtoe operator
 
 // SET FUNCTIONS
 
@@ -72,20 +90,24 @@ void book::setBorrowStatus(bool stat)
 
 // GET FUNCTIONS
 
-const string book::getTitle()
+string book::getTitle() const
 { return title; }//end getTitle
 
-const string book::getAuthor()
+string book::getAuthor() const
 { return author; }//end getAuthor
 
-const int book::getDate()
+int book::getDate() const
 { return datePublished; }//end getDate
 
-const bool book::isBorrowed()
+bool book::isBorrowed() const
 { return borrowed; }//end isBorrowed
 
-const double book::getCatalogNumber()
+double book::getCatalogNumber() const
 { return catalogNumber; }//end getCatalogNumber
+
+// PRINT FUNCTIONS
+void book::printTitle() const
+{ cout << title << endl; }//end printTitle
 
 // CONSTRUCTOR
 
@@ -96,5 +118,18 @@ book::book(string newTitle, string name, int publishDate, double num)
 	setDate(publishDate);
 	setCatalogNumber(num);
 }//end book
+
+book::book(const book& otherBook) 
+{ copyBook(otherBook); }//end copy constructor
+
+// PRIVATE FUNCTIONS
+
+void book::copyBook(const book& otherBook) {
+    title = otherBook.getTitle();
+    author = otherBook.getAuthor();
+    borrowed = otherBook.isBorrowed();
+    datePublished = otherBook.getDate();
+    catalogNumber = otherBook.getCatalogNumber();
+}//end copyBook
 
 #endif

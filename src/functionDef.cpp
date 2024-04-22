538b51d38294653841805712ea52824d4abd9f7e
alyssavitug <149891949+alyssavitug@users.noreply.github.com>
2024-04-22 16:23:19 -0400

    Create functionDef.cpp
    Definition of function prototypes

---
+++ src/functionDef.cpp
@@ -0,0 +1,15 @@
+#include "bookDatabase.h" 
+
+// implement the loadFromFile function
+void bookDatabase::loadFromFile(ifstream inFile) {
+    // implementation to load the address book from a file
+    // this is to read data from the input file stream 'inFile' and populate the book database?
+
+}
+
+// implement the saveToFile function
+void bookDatabase::saveToFile(ofstream outFile) {
+    // implementation to save the address book to a file
+    // write data from the book database to the output file stream 'outFile'
+    // implementation details depend on the file format and data structure used
+}

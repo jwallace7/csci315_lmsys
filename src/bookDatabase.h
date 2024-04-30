4a4e11c4120ca99513e23955feee0bb4bf5566c2
pod-s <trentenpodach@outlook.com>
2024-04-30 16:30:31 -0400

    Updated bookDatabase.h
    Converted bookDatabase.h from an orderedLinkedList to a binarySearchTree. Stored the old bookDatabase.h file in bookDatabaseOLD.h for reference.

--- src/bookDatabase.h
+++ src/bookDatabase.h
@@ -1,57 +1,72 @@
 #ifndef BOOKDATABASE_H
 #define BOOKDATABASE_H
 
-#include <iostream>
-#include <string.h>
-
+#include "binarySearchTree.h"
 #include "book.h"
-#include "orderedLinkedList.h"
 
-class bookDatabase : public orderedLinkedList<book>
+#include <string>
+#include <fstream>
+
+class bookDatabase : bSearchTreeType<book>
 {
 public:
-  book getBookFromTitle(std::string title);
-  // Function that gets a book based on the given title string.
-  // Precondition: The book exists in the book database.
-  // Postcondition: Returns a book if its found, returns an empty book if not.
+    book getBookFromTitle(std::string title);
+    // Function that gets a book based on the given title string.
+    // Precondition: The book exists in the book database.
+    // Postcondition: Returns a book if its found, returns an empty book if not.
 
-  // LOAN FUNCTIONS
-  void printAllLoans();
+    // LOAN FUNCTIONS
+    void printAllLoans() const;
 
-  // DATA MANAGEMENT FUNCTIONS
-  void loadFromFile(ifstream inFile); // *Alyssa* Function to load the address book from a file
-  void saveToFile(ofstream outFile); // *Alyssa* Function to save the address book to a file
+    // DATA MANAGEMENT FUNCTIONS
+    void loadFromFile(ifstream inFile); // *Alyssa* Function to load the address book from a file
+    void saveToFile(ofstream outFile); // *Alyssa* Function to save the address book to a file
+private:
+    void printLoan(nodeType<book> *b) const;
 };
 
+// Custom search function that returns a book if its found, and an empty book when its not.
 book bookDatabase::getBookFromTitle(std::string title) {
-  nodeType<book> *current; // pointer to traverse the linked list
-  current = first;
+    nodeType<book> *current; // Declare a node type of book.
 
-  while (current != nullptr) // While loop
-    if (current->info.getTitle() == title) // If the title matches
-      return current->info; // Return the book
-    else // Otherwise, iterate through the next book
-      current = current->link;
+    if (root == nullptr)
+        cout << "Cannot search an empty tree." << endl;
+    else
+    { 
+       current = root;
 
-  return book(); // If no book is found, return an empty book
+       while (current != nullptr)
+        {
+            if ((current->info).getTitle() == title)
+                return current->info; // Return the book if its found.
+            else if ((current->info).getTitle() > title)
+                current = current->lLink; // If not, move to the next node.
+            else
+                current = current->rLink;
+        }//end while
+    }//end else
+
+    // If the book isn't found, return an empty book.
+    return book();
 }//end getBookFromTitle
 
-void bookDatabase::printAllLoans()
-{
-    nodeType<book>* current; // pointer to traverse the linked list
-    current = first;
+// Custom inorder traversal that prints all loans from books.
+void bookDatabase::printAllLoans() const {
+    printLoan(root);
+}//end printALlLoans
 
-    while (current != nullptr)
+// In order traversal that prints the loan of the book, then iterates to the next book.
+void bookDatabase::printLoan(nodeType<book> *b) const {
+    if (b != nullptr)
     {
-      if (current->info.isBorrowed())
-      {
-        current->info.printTitle();
-        cout << "Current Borrower: " << current->info.currentBorrower() << endl;
-      }
-      current = current->link;
-      cout << endl;
+        printLoan(b->lLink);
+        if (b->info.isBorrowed())
+        {
+            b->info.printTitle();
+            cout << "Current Borrower: " << b->info.currentBorrower() << endl;
+        }
+        printLoan(b->rLink);
     }
-}// end printAllLoans
-
+}//end printLoan
 
 #endif
---
+++ src/bookDatabaseOLD.h
@@ -0,0 +1,57 @@
+#ifndef BOOKDATABASE_H
+#define BOOKDATABASE_H
+
+#include <iostream>
+#include <string.h>
+
+#include "book.h"
+#include "orderedLinkedList.h"
+
+class bookDatabase : public orderedLinkedList<book>
+{
+public:
+  book getBookFromTitle(std::string title);
+  // Function that gets a book based on the given title string.
+  // Precondition: The book exists in the book database.
+  // Postcondition: Returns a book if its found, returns an empty book if not.
+
+  // LOAN FUNCTIONS
+  void printAllLoans();
+
+  // DATA MANAGEMENT FUNCTIONS
+  void loadFromFile(ifstream inFile); // *Alyssa* Function to load the address book from a file
+  void saveToFile(ofstream outFile); // *Alyssa* Function to save the address book to a file
+};
+
+book bookDatabase::getBookFromTitle(std::string title) {
+  nodeType<book> *current; // pointer to traverse the linked list
+  current = first;
+
+  while (current != nullptr) // While loop
+    if (current->info.getTitle() == title) // If the title matches
+      return current->info; // Return the book
+    else // Otherwise, iterate through the next book
+      current = current->link;
+
+  return book(); // If no book is found, return an empty book
+}//end getBookFromTitle
+
+void bookDatabase::printAllLoans()
+{
+    nodeType<book>* current; // pointer to traverse the linked list
+    current = first;
+
+    while (current != nullptr)
+    {
+      if (current->info.isBorrowed())
+      {
+        current->info.printTitle();
+        cout << "Current Borrower: " << current->info.currentBorrower() << endl;
+      }
+      current = current->link;
+      cout << endl;
+    }
+}// end printAllLoans
+
+
+#endif
